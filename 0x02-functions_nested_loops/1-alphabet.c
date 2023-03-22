02eaa240ecaec68dbe5e0254d9d93483f8fcd278
adegboyega <adeyeyegboyega@gmail.comgit config --global user.email adeyeyegboyega@gmail.com>
2023-03-22 16:10:59 -0700

    doing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - all in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char d;
+
+	for (d = 'a' ; d <= 'z' ; d++)
+		_putchar(d);
+	_putchar('\n');
+}
